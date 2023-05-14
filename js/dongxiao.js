91bb59bbfc0d0df12f44c9879a44f3a49739ebac
ma-ziping <126221200+ma-ziping@users.noreply.github.com>
2023-05-14 14:51:19 +0800

    Add files via upload

---
+++ js/dongxiao.js
@@ -0,0 +1,6 @@
+var animation = bodymovin.loadAnimation({
+			container: document.getElementById('animation'),
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			path: 'animation.json' // JSON 动效文件的 URL 地址});
